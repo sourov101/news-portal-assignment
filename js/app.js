183186551250de71147e969243023386000156ac
sourov <sourov.jsr98@gmail.com>
2022-09-03 00:26:28 +0600

    card display function added

--- js/app.js
+++ js/app.js
@@ -26,16 +26,49 @@ const displayCatagory = (data) => {
 
 
 const loadCardId = async () => {
-    const url = `https://openapi.programming-hero.com/api/news/category/01`
+    const url = `https://openapi.programming-hero.com/api/news/category/05`
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data);
+    displayCard(data.data);
 }
 
 
 
+const displayCard = (data) => {
+    const cardContainer = document.getElementById('card-section');
+    data.forEach(info => {
+        const cardDiv = document.createElement('div');
+        cardDiv.innerHTML = `
+        <div class="card mb-3 g-4 shadow" style="max-width: 1500px;">
+        <div class="row g-0">
+        <div class="col-md-4 ">
+            <img src="${info.thumbnail_url}" class="img-fluid rounded-start p-2" alt="...">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title mb-4">${info.title}</h5>
+                <p  class="card-text">${info.details.slice(0, 200)} ...</p>   
+            </div>
+            <div class="card-body">
+            <img src="${info.author.img}" class="img-fluid rounded-circle p-2" style="max-width: 70px;alt="...">
+            <small class="card-text me-5">${info.author.name}</small> 
+            
+            <button class="btn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right text-primary" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+          </svg></button>
+            
+            </div>
 
+        </div>
+        </div> 
+        </div>`;
+
+        cardContainer.appendChild(cardDiv);
+    });
+
+
+}
 
 loadCardId();
 
-loadCatagory();
+loadCatagory(01);
