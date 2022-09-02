4151a4a5f219dbe290c6449ff7a4d688c18c1afb
sourov <sourov.jsr98@gmail.com>
2022-09-02 23:25:02 +0600

    load function added for card

--- js/app.js
+++ js/app.js
@@ -25,4 +25,17 @@ const displayCatagory = (data) => {
 }
 
 
+const loadCardId = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/category/01`
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data);
+}
+
+
+
+
+
+loadCardId();
+
 loadCatagory();
