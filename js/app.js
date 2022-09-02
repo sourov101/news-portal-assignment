de73d18263aaba19043ab0635c7bf144b63e29ec
sourov <sourov.jsr98@gmail.com>
2022-09-02 21:42:12 +0600

    load catagory

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log('added')
+const loadCatagory = () => {
+
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayCatagory(data.data.news_category))
+        .catch(err => alert(err))
+
+}
+
+const displayCatagory = (data) => {
+    console.log(data)
+
+}
+
+
+loadCatagory();
