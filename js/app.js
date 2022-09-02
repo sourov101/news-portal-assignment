bfdbabfc6e9673cf3a3bf8fb1d1470cca1fe10f5
sourov <sourov.jsr98@gmail.com>
2022-09-02 23:07:46 +0600

    display catagory

--- js/app.js
+++ js/app.js
@@ -9,7 +9,18 @@ const loadCatagory = () => {
 }
 
 const displayCatagory = (data) => {
-    console.log(data)
+    const catagorySection = document.getElementById('catagory-div');
+    for (const info of data) {
+        const catagoryDiv = document.createElement('div');
+        catagoryDiv.innerHTML = `
+        <ul class="list-unstyled">
+            <li><a class="text-decoration-none text-secondary px-3 link-primary" href="#">${info.category_name}</a></li>
+        </ul>
+`
+
+        catagorySection.appendChild(catagoryDiv);
+    }
+
 
 }
 
