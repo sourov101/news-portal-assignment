6551730c3f8c8b75621bd91ae3a3fe1a0e58fe5e
sourov <sourov.jsr98@gmail.com>
2022-09-03 11:33:56 +0600

    item  dinamically added

--- js/app.js
+++ js/app.js
@@ -11,12 +11,14 @@ const loadCatagory = () => {
 const displayCatagory = (data) => {
     const catagorySection = document.getElementById('catagory-div');
     for (const info of data) {
+
+
         const catagoryDiv = document.createElement('div');
         catagoryDiv.innerHTML = `
         <ul class="list-unstyled">
-            <li><a onclick="catagoryFunction(${info.category_id})" class="text-decoration-none text-secondary px-3 link-primary" href="#">${info.category_name}</a></li>
-        </ul>
-`
+                   <li><a onclick="catagoryFunction('${info.category_id}')" class="text-decoration-none text-secondary mx-3 py-4 link-primary" href="#">${info.category_name}</a></li>
+        </ul >
+    `
 
         catagorySection.appendChild(catagoryDiv);
     }
@@ -35,6 +37,12 @@ const loadCardId = async (value) => {
 
 
 const displayCard = (data) => {
+    console.log(data)
+    const itemFound = document.getElementById('item-found');
+    itemFound.innerHTML = `<p>${data.length} items found </span></p>`
+
+
+
     const cardContainer = document.getElementById('card-section');
     cardContainer.textContent = '';
     data.forEach(info => {
@@ -47,14 +55,22 @@ const displayCard = (data) => {
         </div>
         <div class="col-md-8">
             <div class="card-body">
-                <h5 class="card-title mb-4">${info.title}</h5>
+                <h5 class="card-title mb-4 fw-semibold">${info.title}</h5>
                 <p  class="card-text">${info.details.slice(0, 200)} ...</p>   
             </div>
             <div class="card-body">
             <img src="${info.author.img}" class="img-fluid rounded-circle p-2" style="max-width: 70px;alt="...">
             <small class="card-text me-5">${info.author.name}</small> 
             
-            <button class="btn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right text-primary" viewBox="0 0 16 16">
+            <small class="card-text me-5">
+            <svg class="me-2 " xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+          </svg>  
+          ${info.total_view}
+          </small> 
+            
+            <button id="arrow-button" class="btn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right text-primary" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
           </svg></button>
             
@@ -71,8 +87,13 @@ const displayCard = (data) => {
 }
 function catagoryFunction(id) {
 
-    const value = '0' + id;
-    loadCardId(value);
+
+    loadCardId(id);
+
+
+
+
+
 }
 
 
